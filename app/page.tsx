98450f17760b26c786098a3bac7a2b8d8b8d48fb
HassanKhan123 <hk260280@gmail.com>
2025-12-27 18:55:28 +0100

    added: suspense and coingecko api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { Suspense } from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import {
+  CategoriesFallback,
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <Suspense fallback={<CategoriesFallback />}></Suspense>
+      </section>
+    </main>
+  );
 };
 
 export default Page;
---
+++ components/home/CoinOverview.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react-hooks/error-boundaries */
+import Image from "next/image";
+import { formatCurrency } from "@/lib/utils";
+// import CandlestickChart from "@/components/CandlestickChart";
+import { fetcher } from "@/lib/coingecko.actions";
+import { CoinOverviewFallback } from "./fallback";
+
+const CoinOverview = async () => {
+  try {
+    const [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+      //   fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+      //     vs_currency: "usd",
+      //     days: 1,
+      //     interval: "hourly",
+      //     precision: "full",
+      //   }),
+    ]);
+
+    return (
+      <div id="coin-overview">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name} / {coin.symbol.toUpperCase()}
+            </p>
+
+            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+          </div>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching coin overview:", error);
+    return <CoinOverviewFallback />;
+  }
+};
+
+export default CoinOverview;
